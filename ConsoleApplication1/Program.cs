2225affcb36e02ae2777f5042d569f9095aea64e
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-02-22 23:57:47 +0900

    d

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shirts shirts = new Shirts
+            {
+                Name = "myName",
+                Created = new DateTime(2020, 10, 20),
+                Sizes = new List<string> { "small", "big" }
+            };
+
+            string json1 = JsonConvert.SerializeObject(shirts, Formatting.Indented);
+
+            Console.WriteLine(json1);
+
+
+            string json2 = @"{
+                              'Name': 'myName',
+                              'Created': '2020-10-20T00:00:00',
+                              'Sizes': [
+                                'small',
+                                'big'
+                              ]
+                            }";
+            Shirts shirts2 = JsonConvert.DeserializeObject<Shirts>(json2);
+            Console.WriteLine("{0} - {1} - {2}", shirts2.Name, shirts2.Created, shirts2.Sizes);
+            Console.WriteLine(shirts2.Sizes[0]);
+            Console.WriteLine(shirts2.Sizes[1]);
+        }
+    }
+}
---
+++ ConsoleApplication1/Shirts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Shirts
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public List<string> Sizes { get; set; }
+    }
+}
